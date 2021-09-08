6fc06022d639ab3ed52aa75395cfb43fad07baeb
Tara Schuchort <tara.schuchort@gmail.com>
2021-09-08 16:56:06 +0200

    Changed Mouselook script to own code
    Changed Mouselook.cs to own code and removes previous ThirdParty script. Player can now look up and down by 180° and look left and right.

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -1,40 +1,39 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 using Cursor = UnityEngine.Cursor;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    // determines in-game mouse sensitivity (could be user input!)
+    public float mouseSensitivity = 500f;
 
     public Transform playerBody;
 
-    // represented via variable and not directly because we need to limit the 
+    //xRotation rotates around x-axis (up and down) and y rotation around y-axis (left and right), standard is 0
     float xRotation = 0f;
-    
-    // Start is called before the first frame update
+    float yRotation = 0f;
+
     void Start()
     {
-        // hide and lock cursor to middle of screen
+        // locks cursor to center of game window and hides it
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // time.deltatime is time gone by since last time update was called,
-        // multiply by this to have mov independent off frame rate
+        // get mouse axis input, apply mouse sensitivity and multiply by Time.deltaTime for frame independence
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // camera moves in direction the mouse moves (change -/+ to invert this)
+        xRotation -= mouseY;
+        yRotation += mouseX;
+        // min-90 to max90 means full rotation, so up/down is limited to 180°
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
         
-        xRotation -= mouseY; // increasing would be flipped, therefore decreasing
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // limit rotation on x axis
-        
-        // transforms the rotation (Quaternions used for internal rotation rep. without gimbal lock!)
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        
+        // 
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
