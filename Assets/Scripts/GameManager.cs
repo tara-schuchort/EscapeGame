b3fed9a38a788a88cc16f6e3de692365a324ae77
lolotta <64040464+lolotta@users.noreply.github.com>
2021-09-26 20:33:33 +0100

    2nd room, hallway, gamemanager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool key1;
+
+    private bool demon_defeated;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        key1 = false;
+        demon_defeated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
