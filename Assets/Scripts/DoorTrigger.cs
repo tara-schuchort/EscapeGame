2699d511c9be93cbacffe9ca669d0cf9a9d0fcc3
Heylene <hpretzler@uni-osnabrueck.de>
2021-09-29 22:52:37 +0200

    die tür nomma fixen
    also, dass da ein loch hinter ist

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -8,31 +8,33 @@ public class DoorTrigger : MonoBehaviour
     public Transform door;
     public float threshhold; 
     private bool door_active; 
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-     
+      threshhold = 1;
       door_active = false; 
       player = GameObject.Find("Player").transform; 
-      door = GameObject.Find("Door").transform; 
+      door = GameObject.Find("Door").transform;
+      anim = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-        
-
-        
-        var dist = Vector3.Distance(player.position,door.position );
+            
+        var dist = Vector3.Distance(player.position,door.position);
 
         if (dist <= threshhold){
-                //dosth
+                
+            door_active = true;
+            Debug.Log("true");
 
-                Debug.Log("true");
+        }
 
+        if (door_active){
+            anim.Play("dooropen");
         }
     }
 }
